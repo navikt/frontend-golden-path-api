7e22e5d75ae8ba53476ba55af427bf8f208574bb
Andreas Nordahl <andnorda@gmail.com>
2024-01-18 10:41:15 +0100

    remove Bearer from token

--- index.ts
+++ index.ts
@@ -1,22 +1,24 @@
-import { validateIdportenToken } from '@navikt/next-auth-wonderwall'
+import { validateIdportenToken } from "@navikt/next-auth-wonderwall";
 
 const server = Bun.serve({
-    port: 3000,
-    async fetch(req) {
-        if (req.url.endsWith("isAlive")) {
-            return new Response("OK");
-        }
+  port: 3000,
+  async fetch(req) {
+    if (req.url.endsWith("isAlive")) {
+      return new Response("OK");
+    }
 
-        const authHeader = req.headers.get('Authorization')
-        if (!authHeader) {
-            return new Response("Authorization header missing", { status: 403 });
-        }
-        const validationResult = await validateIdportenToken(authHeader)
-        if (validationResult !== "valid") {
-            return new Response("Token is not valid", { status: 403 });
-        }
-        return new Response("Bun og greier og sånn!");
-    },
+    const authHeader = req.headers.get("Authorization");
+    if (!authHeader) {
+      return new Response("Authorization header missing", { status: 403 });
+    }
+    const validationResult = await validateIdportenToken(
+      authHeader.replace("Bearer ", "")
+    );
+    if (validationResult !== "valid") {
+      return new Response("Token is not valid", { status: 403 });
+    }
+    return new Response("Bun og greier og sånn!");
+  },
 });
 
 console.log(`Listening on http://localhost:${server.port} ...`);
